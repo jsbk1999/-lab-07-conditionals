6ad20b538dbe8a101c1e0a523b6ee69696e40c16
Jsbk1999 <jsoriano1499@gmail.com>
2021-03-22 19:26:54 -0400

    finished spanish.py

---
+++ spanish.py
@@ -0,0 +1,9 @@
+Spanish = input("Give me a word in English...")
+if Spanish == "cat":
+    print("gato")
+elif Spanish == "dog":
+    print("perro")
+elif Spanish == "horse":
+    print("caballo")
+else:
+    print("no entiendo")
